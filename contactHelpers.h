a4682854c4baa08e4701b4c7f3709432c1b7bab6
DrChinaWhite <drhawkins@mta.ca>
2020-11-20 21:20:20 -0500

    Create contactHelpers.h

---
+++ contactHelpers.h
@@ -0,0 +1,46 @@
+//==============================================
+// Name:           
+// Student Number: #########
+// Email:          @myseneca.ca
+// Section:        
+// Date:           Nov 20 2020
+//==============================================
+// Assignment:     2
+// Milestone:      2
+//==============================================
+
+// +-------------------------------------------------+
+// | NOTE:  Copy/Paste your Assignment-2 Milestone-1 |
+// |        contents below...                        |
+// +-------------------------------------------------+
+
+//--------------------------------
+// Function Prototypes
+//--------------------------------
+
+// Clear the standard input buffer
+void clearKeyboard(void);
+
+// pause:
+// Pause execution of function
+void pause(void);
+
+// getInt:
+// Returns a valid interger
+int getInt(void);
+
+// getIntInRange:
+// Returns a valid interger in range
+int getIntInRange(int min, int max);
+
+// yes:
+// Prompts user for single character
+int yes(void);
+
+// menu:
+// Prompt user for valid interger between 0-6 inclusive
+int menu(void);
+
+// contactManagerSystem:
+// Shows system menu and recieves user selection
+void contactManagerSystem(void);
